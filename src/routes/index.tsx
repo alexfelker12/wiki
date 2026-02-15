f4c8bed7901c3f3b519e088fe750a18b63666b87
Alex Felker <alexanderfelker@gmx.net>
2026-02-15 23:51:24 +0100

    adding ORM and deployment code - added prisma as database ORM - added netlify vite plugin for deployment - added test query in root to check if orm is working

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,27 @@ import { createFileRoute } from "@tanstack/react-router";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { createServerFn } from "@tanstack/react-start";
+import { db } from "@/lib/db";
 
-export const Route = createFileRoute("/")({ component: App });
+export const Route = createFileRoute("/")({
+  component: App,
+  loader: async () => await getData()
+})
+
+const getTestData = async () => {
+  return db.test.findMany()
+}
+
+const getData = createServerFn({
+  method: 'GET',
+}).handler(() => {
+  return getTestData()
+})
 
 function App() {
+  const testData = Route.useLoaderData()
+
   return (
     <div className="p-4">
       <Card size="sm" className="mx-auto w-full max-w-sm">
@@ -16,9 +33,11 @@ function App() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <p>
-            Some more random text content to see the Card component in use
-          </p>
+          <ul>
+            {testData.map(data => (
+              <li key={data.id}>{data.value}</li>
+            ))}
+          </ul>
         </CardContent>
         <CardFooter>
           <Button variant="outline" size="sm" className="w-full">
